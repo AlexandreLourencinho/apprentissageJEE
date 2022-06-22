d53dac4f7ddb44fcb010957b1b9670763d6ae9f1
Alexandre <alexandre.lourencinho@gmail.com>
2022-06-22 16:05:45 +0200

    feat: methods findBy account id, with and without pageable

--- ebanking/ebanking-backend/src/main/java/fr/loual/ebankingbackend/repositories/BankAccountOperationRepository.java
+++ ebanking/ebanking-backend/src/main/java/fr/loual/ebankingbackend/repositories/BankAccountOperationRepository.java
@@ -1,8 +1,14 @@
 package fr.loual.ebankingbackend.repositories;
 
 import fr.loual.ebankingbackend.entities.AccountOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface BankAccountOperationRepository extends JpaRepository<AccountOperation, Long> {
 
+    List<AccountOperation> findByBankAccountId(String accountId);
+    Page<AccountOperation> findByBankAccountId(String accountId, Pageable pageable);
 }
