bbe602578c7d9c06555324304ffedbe1d7d71b57
Alexandre <alexandre.lourencinho@gmail.com>
2022-06-27 15:34:40 +0200

    feat: DTOs for operations

---
+++ ebanking/ebanking-backend/src/main/java/fr/loual/ebankingbackend/dtos/TransfertRequestDTO.java
@@ -0,0 +1,14 @@
+package fr.loual.ebankingbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class TransfertRequestDTO {
+
+    private String accountSource;
+    private String accountDestination;
+    private double amount;
+    private String description;
+
+
+}
