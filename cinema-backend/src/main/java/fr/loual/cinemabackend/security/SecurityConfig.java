25fd2e10ab6a3bbc6b0b54bf6f8869de7b32aaef
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-05 20:26:45 +0200

    feat: Security config

---
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package fr.loual.cinemabackend.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration @AllArgsConstructor
+public class SecurityConfig {
+
+}
