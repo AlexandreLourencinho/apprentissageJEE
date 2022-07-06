bcb80b2fc9924411c3312a60a9983c3a92962d6a
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-06 17:53:17 +0200

    feat: constants in JWTUtils

--- cinema-backend/src/main/java/fr/loual/cinemabackend/security/JWTUtils.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/security/JWTUtils.java
@@ -1,5 +1,7 @@
 package fr.loual.cinemabackend.security;
 
 public class JWTUtils {
-
+    public static final String HEADER = "Authorization";
+    public static final String  SECRET_KEY = "Alpo087IsutKal";
+    public static final String  PREFIX = "Bearer ";
 }
