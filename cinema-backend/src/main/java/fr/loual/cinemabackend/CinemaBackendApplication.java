c4a8e5d10e410852eb82809389aa3bf8867eb1b9
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-06 17:48:17 +0200

    feat: adding password Encoder bean

--- cinema-backend/src/main/java/fr/loual/cinemabackend/CinemaBackendApplication.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/CinemaBackendApplication.java
@@ -2,7 +2,10 @@ package fr.loual.cinemabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -10,6 +13,11 @@ public class CinemaBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaBackendApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
