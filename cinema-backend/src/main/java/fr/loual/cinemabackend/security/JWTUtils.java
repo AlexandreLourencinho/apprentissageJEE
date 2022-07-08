db3a56ae84ad29976649975a15bba8364f17fd24
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-08 15:09:59 +0200

    fix: added constants for token's expiration and secret key

--- cinema-backend/src/main/java/fr/loual/cinemabackend/security/JWTUtils.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/security/JWTUtils.java
@@ -4,4 +4,6 @@ public class JWTUtils {
     public static final String HEADER = "Authorization";
     public static final String  SECRET_KEY = "Alpo087IsutKal";
     public static final String  PREFIX = "Bearer ";
+    public static final int  EXPIRE_ACCESS_TOKEN = 900000;
+    public static final int  EXPIRE_REFRESH_TOKEN = 3600000;
 }
