b02ceb98a1ab6dc00158b734fe4605a654145c02
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-13 17:39:29 +0200

    feat: added findByName

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/CityRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/CityRepository.java
@@ -4,4 +4,6 @@ import fr.loual.cinemabackend.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    City findByName(String name);
 }
