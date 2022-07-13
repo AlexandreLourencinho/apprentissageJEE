a093cc0f1cf2532848413792d5f057f9244a09d7
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-13 17:41:50 +0200

    fix: changed parameters name to reflect their DTO status

--- cinema-backend/src/main/java/fr/loual/cinemabackend/services/MovieProjectionService.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/services/MovieProjectionService.java
@@ -1,24 +1,25 @@
 package fr.loual.cinemabackend.services;
 
 import fr.loual.cinemabackend.dtos.*;
+import fr.loual.cinemabackend.exceptions.*;
 
 import java.util.Date;
 import java.util.List;
 
 public interface MovieProjectionService {
 
-    CityDTO addCity(CityDTO city);
-    CinemaDTO addCinema(CinemaDTO cinema);
-    CategoryDTO addCategory(CategoryDTO category);
-    MovieDTO addMovie(MovieDTO movie);
-    RoomDTO addRoomToCinema(RoomDTO room);
-    PlaceDTO addPlaceToRoom(PlaceDTO place);
-    TicketDTO addTicket(TicketDTO ticket);
-    SeanceDTO addSeance(SeanceDTO seance);
+    CityDTO addCity(CityDTO cityDTO) throws InvalidCityArgumentException, CityAlreadyRegisteredException;
+    CinemaDTO addCinema(CinemaDTO cinemaDTO) throws InvalidCinemaArgumentException, CinemaAlreadyExistsException;
+    CategoryDTO addCategory(CategoryDTO categoryDTO) throws InvalidCategoryArgumentException, CategoryAlreadyExistsException;
+    MovieDTO addMovie(MovieDTO movieDTO);
+    RoomDTO addRoomToCinema(RoomDTO roomDTO);
+    PlaceDTO addPlaceToRoom(PlaceDTO placeDTO);
+    TicketDTO addTicket(TicketDTO ticketDTO);
+    SeanceDTO addSeance(SeanceDTO seanceDTO);
 
     List<CityDTO> getCities();
-    List<CinemaDTO> getCinemaByCity(CityDTO city);
-    List<SeanceDTO> getSeanceForMovie(MovieDTO movie, Date date);
+    List<CinemaDTO> getCinemaByCity(CityDTO cityDTO);
+    List<SeanceDTO> getSeanceForMovie(MovieDTO movieDTO, Date date);
 
 
 }
