25397867a769c24211d393c5be9d278062ff5fd4
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-13 17:40:52 +0200

    feat: added findByAltitudeAndLatitudeAndLongitude

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/CinemaRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/CinemaRepository.java
@@ -4,4 +4,7 @@ import fr.loual.cinemabackend.entities.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CinemaRepository extends JpaRepository<Cinema, String> {
+
+    Cinema findByAltitudeAndLatitudeAndLongitude(double altitude, double latitude, double longitude);
+
 }
