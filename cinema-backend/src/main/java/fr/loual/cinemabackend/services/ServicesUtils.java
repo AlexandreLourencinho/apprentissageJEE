26e5f6a9158fb5f7dab2d6d99af4d4dc5bed32a5
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-15 22:27:10 +0200

    feat: services utils with error message generators

---
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/services/ServicesUtils.java
@@ -0,0 +1,17 @@
+package fr.loual.cinemabackend.services;
+
+public class ServicesUtils {
+
+    public static String exceptionMessage(String message) {
+        return String.format("Les champs nécessaires à l'ajout d'%s n'ont pas été remplis.", message);
+    }
+
+    public static String alreadyExistsErrorMessage(String message) {
+        return String.format("%s que vous avez essayé d'ajouter est déjà répertorié.", message);
+    }
+
+    public static String infoMessage(String type, String name) {
+        return String.format("-- %s %s à bien été enregistré --",type, name);
+    }
+
+}
