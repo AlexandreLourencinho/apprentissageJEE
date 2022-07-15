d7ed403672eb16c6d7b05500112b277a17669098
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-15 22:27:50 +0200

    feat: add methods for add movie management

--- cinema-backend/src/main/java/fr/loual/cinemabackend/services/MovieProjectionServiceImpl.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/services/MovieProjectionServiceImpl.java
@@ -4,9 +4,10 @@ import fr.loual.cinemabackend.dtos.*;
 import fr.loual.cinemabackend.entities.Category;
 import fr.loual.cinemabackend.entities.Cinema;
 import fr.loual.cinemabackend.entities.City;
+import fr.loual.cinemabackend.entities.Movie;
 import fr.loual.cinemabackend.exceptions.*;
 import fr.loual.cinemabackend.repositories.*;
-import fr.loual.cinemabackend.security.mappers.CinemaMapper;
+import fr.loual.cinemabackend.mappers.CinemaMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -35,45 +36,52 @@ public class MovieProjectionServiceImpl implements MovieProjectionService {
 
     @Override
     public CityDTO addCity(CityDTO cityDTO) throws InvalidCityArgumentException, CityAlreadyRegisteredException {
-        if(!dtoAndEntityCheck.checkCityDTO(cityDTO)) throw new InvalidCityArgumentException("Tous les champs nécessaires à l'ajout d'une ville n'ont pas été remplis.");
+        if(!dtoAndEntityCheck.checkCityDTO(cityDTO)) throw new InvalidCityArgumentException(ServicesUtils.exceptionMessage("une ville"));
         City cityRepo = cityRepository.findByName(cityDTO.getName());
-        if(cityRepo != null) throw new CityAlreadyRegisteredException("La ville à déjà été ajoutée.");
+        if(cityRepo != null) throw new CityAlreadyRegisteredException(ServicesUtils.alreadyExistsErrorMessage("La ville"));
         log.info("--Enregistrement d'une nouvelle ville...-");
         City city = mapper.cityDTOToCity(cityDTO);
         cityRepository.save(city);
-        log.info("--La ville " + city.getName() + " à bien été enregistrée.--");
+        log.info(ServicesUtils.infoMessage("La ville", city.getName()));
         return mapper.cityToCityDTO(city);
     }
 
     @Override
     public CinemaDTO addCinema(CinemaDTO cinemaDTO) throws InvalidCinemaArgumentException, CinemaAlreadyExistsException {
-        if(!dtoAndEntityCheck.checkCinemaDTO(cinemaDTO)) throw new InvalidCinemaArgumentException("Tous les champs nécessaires à l'ajout d'un cinéma n'ont pas été remplis.");
+        if(!dtoAndEntityCheck.checkCinemaDTO(cinemaDTO)) throw new InvalidCinemaArgumentException(ServicesUtils.exceptionMessage("un cinéma"));
         Cinema cinemaRepo = cinemaRepository.findByAltitudeAndLatitudeAndLongitude(cinemaDTO.getAltitude(), cinemaDTO.getLatitude(), cinemaDTO.getLongitude());
-        if(cinemaRepo != null) throw new CinemaAlreadyExistsException("Le cinéma que vous avez essayé d'ajouter est déjà répertorié.");
+        if(cinemaRepo != null) throw new CinemaAlreadyExistsException(ServicesUtils.alreadyExistsErrorMessage("Le cinéma"));
         log.info("--Enregistrement d'un nouveau cinéma...--");
         Cinema cinema = mapper.cinemaDTOToCinema(cinemaDTO);
         cinema.setId(UUID.randomUUID().toString());
         cinemaRepository.save(cinema);
-        log.info("--Le cinéma " + cinema.getName() + " à bien été enregistré--");
+        log.info(ServicesUtils.infoMessage("Le cinéma", cinema.getName()));
         return mapper.cinemaToCinemaDTO(cinema);
     }
 
     @Override
     public CategoryDTO addCategory(CategoryDTO categoryDTO) throws InvalidCategoryArgumentException, CategoryAlreadyExistsException {
-        if(!dtoAndEntityCheck.checkCategoryDTO(categoryDTO)) throw new InvalidCategoryArgumentException("Les champs nécessaires à l'ajout d'une catégorie n'ont pas été remplis.");
+        if(!dtoAndEntityCheck.checkCategoryDTO(categoryDTO)) throw new InvalidCategoryArgumentException(ServicesUtils.exceptionMessage("une catégorie"));
         Category categoryRepo = categoryRepository.findByName(categoryDTO.getName());
-        if(categoryRepo != null) throw new CategoryAlreadyExistsException("La catégorie que vous avez essayé d'ajouter est déjà répertoriée.");
+        if(categoryRepo != null) throw new CategoryAlreadyExistsException(ServicesUtils.alreadyExistsErrorMessage("La catégorie"));
         log.info("--Enregistrement d'une nouvelle catégorie...--");
         Category category = mapper.categoryDTOToCategory(categoryDTO);
         categoryRepository.save(category);
-        log.info("--La catégorie " + category.getName() + " à bien été enregistrée.--");
+        log.info(ServicesUtils.infoMessage("La catégorie", category.getName()));
         return mapper.categoryToCategoryDTO(category);
     }
 
     @Override
-    public MovieDTO addMovie(MovieDTO movie) {
-        return null;
-    }
+    public MovieDTO addMovie(MovieDTO movieDTO) throws InvalidMovieArgumentException, MovieAlreadyExistsException {
+        if(!dtoAndEntityCheck.checkMovieDTO(movieDTO)) throw new InvalidMovieArgumentException(ServicesUtils.exceptionMessage("un film"));
+        Movie movieRepo = movieRepository.findByTitleAndMakerAndReleaseDate(movieDTO.getTitle(), movieDTO.getMaker(), movieDTO.getReleaseate());
+        if (movieRepo != null) throw new MovieAlreadyExistsException(ServicesUtils.alreadyExistsErrorMessage("Le film"));
+        log.info("--Enregistrement d'un nouveau film...--");
+        Movie movie = mapper.movieDTOToMovie(movieDTO);
+        movieRepository.save(movie);
+        log.info(ServicesUtils.infoMessage("Le film", movie.getTitle()));
+        return mapper.movieToMovieDTO(movie);
+    } // TODO externaliser? code qui se répète
 
     @Override
     public RoomDTO addRoomToCinema(RoomDTO room) {
