e827a9326b5ec504a14401472b9eca9973ea74ba
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-15 22:29:44 +0200

    feat: add methods to find a movie By Title And Maker And ReleaseDate(combo of these 3 should be unique, need to recheck)

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/MovieRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/MovieRepository.java
@@ -3,5 +3,10 @@ package fr.loual.cinemabackend.repositories;
 import fr.loual.cinemabackend.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+
 public interface MovieRepository extends JpaRepository<Movie, String> {
+
+    Movie findByTitleAndMakerAndReleaseDate(String title, String maker, Date releaseDate);
+
 }
