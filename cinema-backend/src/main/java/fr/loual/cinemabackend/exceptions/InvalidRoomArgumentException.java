fc8a3e86f0f62b0bf7d752d74a15cfcf4f746eee
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-17 22:19:10 +0200

    feat: add exception for invalid room argument

---
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/exceptions/InvalidRoomArgumentException.java
@@ -0,0 +1,7 @@
+package fr.loual.cinemabackend.exceptions;
+
+public class InvalidRoomArgumentException extends Exception {
+    public InvalidRoomArgumentException(String message) {
+        super(message);
+    }
+}
