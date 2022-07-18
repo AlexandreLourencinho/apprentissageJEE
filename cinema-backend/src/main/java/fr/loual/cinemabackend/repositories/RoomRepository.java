d360ff87f558761b5d553869a1edbdc28481dcb7
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-18 17:46:55 +0200

    feat: add find by room name and cinema, should be unique

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/RoomRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/RoomRepository.java
@@ -1,7 +1,11 @@
 package fr.loual.cinemabackend.repositories;
 
+import fr.loual.cinemabackend.entities.Cinema;
 import fr.loual.cinemabackend.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, String> {
+
+    Room findByNameAndCinema(String name, Cinema cinema);
+
 }
