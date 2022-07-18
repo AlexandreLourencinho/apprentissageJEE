dc4abece6173123a5c04d2d339bec5ca10dc632a
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-18 18:01:01 +0200

    fix: exception name changed for coherence. will be wrapped with other exceptions

--- cinema-backend/src/main/java/fr/loual/cinemabackend/exceptions/CityAlreadyRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package fr.loual.cinemabackend.exceptions;
-
-public class CityAlreadyRegisteredException extends Exception {
-    public CityAlreadyRegisteredException(String message) {
-        super(message);
-    }
-}
