c737af17660df80cc801dddcdf4afefcee3d8c6d
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-24 23:09:20 +0200

    fix: changing mapper dto methods name

--- cinema-backend/src/main/java/fr/loual/cinemabackend/mappers/CinemaMapper.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/mappers/CinemaMapper.java
@@ -7,15 +7,15 @@ import org.mapstruct.Mapper;
 @Mapper
 public interface CinemaMapper {
 
-    City cityDTOToCity(CityDTO cityDTO);
-    CityDTO cityToCityDTO(City city);
-    Cinema cinemaDTOToCinema(CinemaDTO cinemaDTO);
-    CinemaDTO cinemaToCinemaDTO(Cinema cinema);
-    Category categoryDTOToCategory(CategoryDTO categoryDTO);
-    CategoryDTO categoryToCategoryDTO(Category category);
-    Movie movieDTOToMovie(MovieDTO movieDTO);
-    MovieDTO movieToMovieDTO(Movie movie);
-    Room roomDTOToRoom(RoomDTO roomDTO);
-    RoomDTO roomToRoomDTO(Room room);
+    City DTOToCity(CityDTO cityDTO);
+    CityDTO cityToDTO(City city);
+    Cinema DTOToCinema(CinemaDTO cinemaDTO);
+    CinemaDTO cinemaToDTO(Cinema cinema);
+    Category DTOToCategory(CategoryDTO categoryDTO);
+    CategoryDTO categoryToDTO(Category category);
+    Movie DTOToMovie(MovieDTO movieDTO);
+    MovieDTO movieToDTO(Movie movie);
+    Room DTOToRoom(RoomDTO roomDTO);
+    RoomDTO roomToDTO(Room room);
 
 }
