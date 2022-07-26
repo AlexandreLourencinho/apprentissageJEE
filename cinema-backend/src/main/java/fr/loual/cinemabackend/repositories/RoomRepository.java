dd8665c652871a5b9bc2d7e980b7243188beef7c
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-26 14:41:40 +0200

    feat: add roomlist method

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/RoomRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/RoomRepository.java
@@ -4,8 +4,11 @@ import fr.loual.cinemabackend.entities.Cinema;
 import fr.loual.cinemabackend.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, String> {
 
     Room findByNameAndCinema(String name, Cinema cinema);
+    List<Room> findByCinema(Cinema cinema);
 
 }
