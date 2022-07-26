2076cf3e95f8c9e1fa535cee96e18ceccfd21b41
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-26 14:41:09 +0200

    feat: add find by movie cinema id and begin date

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/MovieProjectionRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/MovieProjectionRepository.java
@@ -1,7 +1,15 @@
 package fr.loual.cinemabackend.repositories;
 
+import fr.loual.cinemabackend.entities.Movie;
+import fr.loual.cinemabackend.entities.Room;
 import fr.loual.cinemabackend.entities.Seance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
+import java.util.List;
+
 public interface MovieProjectionRepository extends JpaRepository<Seance, String> {
+
+    List<Seance> findByMovieAndRoomCinema_IdAndBeginDateIsGreaterThan(Movie movie, String cinemaId, Date beginDate);
 }
