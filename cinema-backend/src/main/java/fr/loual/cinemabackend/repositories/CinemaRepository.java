ceed9a3a063b031bc2c9cc6abc3f72bc4eb913ac
Alexandre <alexandre.lourencinho@gmail.com>
2022-07-26 14:42:06 +0200

    feat: add find by city method

--- cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/CinemaRepository.java
+++ cinema-backend/src/main/java/fr/loual/cinemabackend/repositories/CinemaRepository.java
@@ -1,10 +1,14 @@
 package fr.loual.cinemabackend.repositories;
 
 import fr.loual.cinemabackend.entities.Cinema;
+import fr.loual.cinemabackend.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CinemaRepository extends JpaRepository<Cinema, String> {
 
     Cinema findByAltitudeAndLatitudeAndLongitude(double altitude, double latitude, double longitude);
+    List<Cinema> findByCity(City city);
 
 }
